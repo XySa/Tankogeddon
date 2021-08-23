88a0a917a43afbefcbf7d34c39b00085c116f2dd
XySa <Khusan.yadgarov@gmail.com>
2021-08-23 20:24:04 +0300

    Added tank and turret rotation and cannon mock

--- Source/Tankogeddon/TankPawn.cpp
+++ Source/Tankogeddon/TankPawn.cpp
@@ -5,47 +5,72 @@
 #include <Components/StaticMeshComponent.h>
 #include <GameFramework/SpringArmComponent.h>
 #include <Camera/CameraComponent.h>
+#include <Math/UnrealMathUtility.h>
+#include <Kismet/KismetMathLibrary.h>
+#include <Components/ArrowComponent.h>
 
-// Sets default values
+#include "Tankogeddon.h"
+#include "TankPlayerController.h"
+#include "Cannon.h"
+
 // Sets default values
 ATankPawn::ATankPawn()
 {
-	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Tank body"));
 	RootComponent = BodyMesh;
 
-	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Tank turret"));
-	TurretMesh->SetupAttachment(BodyMesh);
+    TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Tank turret"));
+    TurretMesh->SetupAttachment(BodyMesh);
 
-	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring arm"));
-	SpringArm->SetupAttachment(BodyMesh);
-	SpringArm->bDoCollisionTest = false;
-	SpringArm->bInheritPitch = false;
-	SpringArm->bInheritYaw = false;
-	SpringArm->bInheritRoll = false;
+    CannonSetupPoint = CreateDefaultSubobject<UArrowComponent>(TEXT("Cannon setup point"));
+    CannonSetupPoint->AttachToComponent(TurretMesh, FAttachmentTransformRules::KeepRelativeTransform);
 
-	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-	Camera->SetupAttachment(SpringArm);
+    SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring arm"));
+    SpringArm->SetupAttachment(BodyMesh);
+    SpringArm->bDoCollisionTest = false;
+    SpringArm->bInheritPitch = false;
+    SpringArm->bInheritYaw = false;
+    SpringArm->bInheritRoll = false;
+
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(SpringArm);
 }
 
 void ATankPawn::MoveForward(float AxisValue)
 {
-	TargetForwardAxisValue = AxisValue;
+    TargetForwardAxisValue = AxisValue;
 }
 
-void ATankPawn::MoveRight(float AxisValue)
+void ATankPawn::RotateRight(float AxisValue)
 {
-	TargetRightAxisValue = AxisValue;
+    TargetRightAxisValue = AxisValue;
 }
-
 
 // Called when the game starts or when spawned
 void ATankPawn::BeginPlay()
 {
 	Super::BeginPlay();
 	
+    TankController = Cast<ATankPlayerController>(GetController());
+    SetupCannon();
+}
+
+void ATankPawn::SetupCannon()
+{
+    if (Cannon)
+    {
+        Cannon->Destroy();
+        Cannon = nullptr;
+    }
+
+    FActorSpawnParameters Params;
+    Params.Instigator = this;
+    Params.Owner = this;
+    Cannon = GetWorld()->SpawnActor<ACannon>(CannonClass, Params);
+    Cannon->AttachToComponent(CannonSetupPoint, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
 }
 
 // Called every frame
@@ -53,13 +78,52 @@ void ATankPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector CurrentLocation = GetActorLocation();
-	FVector ForwardVector = GetActorForwardVector();
-	FVector RightVector = GetActorRightVector();
-	FVector MovePosition = CurrentLocation + ForwardVector * MoveSpeed * TargetForwardAxisValue * DeltaTime;
-	FVector MovePosition2 = MovePosition + RightVector * MoveSpeed * TargetRightAxisValue * DeltaTime;
-	SetActorLocation(MovePosition, true);
-	SetActorLocation(MovePosition2, true);
+    // Tank movement
+    CurrentForwardAxisValue = FMath::FInterpTo(CurrentForwardAxisValue, TargetForwardAxisValue, DeltaTime, MovementSmootheness);
+    
+    FVector CurrentLocation = GetActorLocation();
+    FVector ForwardVector = GetActorForwardVector();
+    FVector MovePosition = CurrentLocation + ForwardVector * CurrentForwardAxisValue * MoveSpeed * DeltaTime;
+
+    SetActorLocation(MovePosition, true);
+
+    // Tank rotation
+    CurrentRightAxisValue = FMath::FInterpTo(CurrentRightAxisValue, TargetRightAxisValue, DeltaTime, RotationSmootheness);
+
+    UE_LOG(LogTankogeddon, Verbose, TEXT("CurrentRightAxisValue = %f TargetRightAxisValue = %f"), CurrentRightAxisValue, TargetRightAxisValue);
+
+    FRotator CurrentRotation = GetActorRotation();
+    float YawRotation = CurrentRightAxisValue * RotationSpeed * DeltaTime;
+    YawRotation += CurrentRotation.Yaw;
+
+    FRotator NewRotation = FRotator(0.f, YawRotation, 0.f);
+    SetActorRotation(NewRotation);
+
+    // Turret rotation
+    if (TankController)
+    {
+        FVector MousePos = TankController->GetMousePos();
+        FRotator TargetRotation = UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), MousePos);
+        FRotator CurrRotation = TurretMesh->GetComponentRotation();
+        TargetRotation.Pitch = CurrRotation.Pitch;
+        TargetRotation.Roll = CurrRotation.Roll;
+        TurretMesh->SetWorldRotation(FMath::RInterpConstantTo(CurrRotation, TargetRotation, DeltaTime, TurretRotationSpeed));
+    }
 }
 
+void ATankPawn::Fire()
+{
+    if (Cannon)
+    {
+        Cannon->Fire();
+    }
+}
+
+void ATankPawn::FireSpecial()
+{
+    if (Cannon)
+    {
+        Cannon->FireSpecial();
+    }
+}
 
